19e8112ea967287702d85c570a17ca5ad5019dcb
caipiaoping <125465729@qq.com>
2018-07-31 00:45:59 +0800

    1

--- src/main/java/org/example/HomeController.java
+++ src/main/java/org/example/HomeController.java
@@ -22,7 +22,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import sun.jvm.hotspot.utilities.Assert;
 
 @RestController
 public class HomeController {
