1066ed36e9a2313d838ee923374c5a6c5f91a302
caipiaoping <125465729@qq.com>
2018-07-31 09:54:04 +0800

    1

--- src/main/java/org/example/HomeController.java
+++ src/main/java/org/example/HomeController.java
@@ -15,9 +15,6 @@
  */
 package org.example;
 
-import org.example.domain.User;
-import org.example.domain.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -29,15 +26,9 @@ public class HomeController {
     @Value("${welcome}")
     private String welcome;
 
-    @Autowired
-    private UserRepository userRepository;
-
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String ipaddress() throws Exception {
 
-        userRepository.save(new User(1L, "didi", 30));
-        userRepository.save(new User(2L, "mama", 40));
-        userRepository.save(new User(3L, "kaka", 50));
-        return "Reply: " +  +userRepository.findAll().size();
+        return "Reply: " +  welcome;
     }
 }
---
+++ src/main/java/org/example/UserController.java
@@ -0,0 +1,50 @@
+/**
+ *  Copyright 2005-2016 Red Hat, Inc.
+ *
+ *  Red Hat licenses this file to you under the Apache License, version
+ *  2.0 (the "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *  implied.  See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+package org.example;
+
+import org.example.domain.User;
+import org.example.domain.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RequestMapping("/user")
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @RequestMapping(value = "/{id}", method = { RequestMethod.GET })
+    public User get(@PathVariable("id") Long id) {
+        return userRepository.findOne(id);
+    }
+
+    @RequestMapping(value = "/add/{name}", method = { RequestMethod.GET })
+    public User post(@PathVariable(" name") String name) {
+        return userRepository.save(new User(new Random().nextLong(), name, 30));
+    }
+
+
+    @RequestMapping(value = "/id", method = { RequestMethod.DELETE })
+    public void delete(@PathVariable(" id") Long id) {
+         userRepository.delete(id);
+    }
+}
